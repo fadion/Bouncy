bde6ea4f70c1c748d219f6ccd65a44a3696e594d
Fadion Dashi <jonidashi@gmail.com>
2014-10-29 21:39:26 +0100

    Add a missing comma in config

--- src/config/config.php
+++ src/config/config.php
@@ -11,7 +11,7 @@ return array(
     | else meaningful.
     |
     */
-    'index' => 'default'
+    'index' => 'default',
 
     /*
     |--------------------------------------------------------------------------
